b47c03e97ee2695dcfdc08eeb7db3be5753c5e8d
Raghav Krishna <raghav@Raghavs-MacBook-Air.local>
2025-05-06 17:50:31 +0530

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
 "use client";
-
+import { useEffect } from "react";
 import { Curve, Ready, Marquee } from "@/components";
 import { About, Clients, Hero, Projects } from "@/container";
 import { X } from "@/container";
 
 
 export default function Home() {
+
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll({
+      
+      
+        // Add other options as needed
+      });
+      // Optionally, you can destroy the instance when the component unmounts
+      return () => {
+        locomotiveScroll.destroy();
+      };
+    })();
+  }, []);
  
 
   return (
